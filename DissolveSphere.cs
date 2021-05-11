edc8d6bd70773a9c345a8699ac352af32dd03617
davidarturolr <31114498+davidarturolr@users.noreply.github.com>
2021-05-10 23:29:32 -0500

    Shaders con modificaciones y scripts usados

---
+++ DissolveSphere.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DissolveSphere : MonoBehaviour {
+
+    Material mat;
+
+    private void Start()
+    {
+        mat = GetComponent<Renderer>().material;
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        mat.SetFloat("_DissolveAmount", Mathf.Sin(Time.time) / 2 + 0.1f);
+    }
+
+}
